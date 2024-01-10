4d8b1d7f2d980e33e36e7f611ed3595330942675
Artem Moiseev <moiseev@physics.msu.ru>
2024-01-10 12:36:17 +0300

    Initial Commit

---
+++ SaveDataProductionBootcamp/SaveDataProductionBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveDataProductionBootcampApp.swift
+//  SaveDataProductionBootcamp
+//
+//  Created by Artem on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SaveDataProductionBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
